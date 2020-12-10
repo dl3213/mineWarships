673c0120223b39e05e672ffa06179919d9826d54
dl3213 <dl3213@192.168.0.101>
2020-12-10 21:55:21 +0800

    update

---
+++ Assets/c#/test/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //允许子类重写 start
+    protected virtual void Start()
+    {
+        print("base");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/c#/test/sun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : BaseTest
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     print("sun");
+    // }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        //sun sdo
+        print("sun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void load()
+    {
+        //通过代码读取资源
+        GameObject go = Resources.Load<GameObject>("目录/资源名称");
+
+        //创建资源
+        // Instantiate(go);
+
+        
+    }
+}
